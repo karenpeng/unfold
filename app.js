990daf6cc84969f73e7e5009298e2edf99a26f3e
Karen <karenpenglabs@gmail.com>
2015-02-13 00:28:56 -0500

    weird

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ var callSiteCode = [
   "callsite.getLineNumber(), callsite.getColumnNumber()); ",
   "})",
   "counter++;"
-]
+].join('\n');
 
-function joinCallSiteCode() {
-  var endStr = '';
-  endStr += '\n';
-  callSiteCode.forEach(function (code) {
-    endStr += code.concat('\n');
-  });
-  return endStr;
-}
+// function joinCallSiteCode() {
+//   var endStr = '';
+//   endStr += '\n';
+//   callSiteCode.forEach(function (code) {
+//     endStr += code.concat('\n');
+//   });
+//   return endStr;
+// }
 
 function inject(strArray) {
   var functionRe = /function\s\S*\(\S*\)\{/;
@@ -41,7 +41,7 @@ function inject(strArray) {
       //console.log(functionLine)
       var postStr = str.split(functionRe);
       //console.log(postStr)
-      postStr = functionLine.concat(postStr[0]).concat('\n').concat(joinCallSiteCode()).concat(postStr[1]).concat('\n');
+      postStr = functionLine.concat(postStr[0]).concat('\n').concat(callSiteCode).concat(postStr[1]).concat('\n');
       endStr += postStr;
     } else {
       endStr += str.concat('\n');
@@ -58,7 +58,7 @@ function inject(strArray) {
 }
 
 function quickTest() {
-  var str = "sddg"
+  var str = 'sddg'
   var functionRe = /function\s\S*\(\S*\)\{/
   if (str.match(functionRe) !== null) {
     var functionLine = str.match(functionRe)[0];
@@ -75,7 +75,7 @@ function evaluate(str) {
     eval(str);
     console.log('evaluation done!');
   } catch (ಠ‿ಠ) {
-    console.log("ಠ‿ಠ " + ಠ‿ಠ);
+    console.log('ಠ‿ಠ ' + ಠ‿ಠ);
   }
 }
 
@@ -98,16 +98,16 @@ var fibStr = [
 evaluate(inject(fibStr));
 
 // function fib(num) {
-
 //   stack().forEach(function (callsite, i) {
 //     console.log('%s counter: %s, level: %s, type: %s, function: %s, method: %s, filename: %s, line: %s, column: %s',
 //       new Array(i + 1).join(' '), counter, i, callsite.getTypeName(), callsite.getFunctionName(), callsite.getMethodName(), callsite.getFileName(),
 //       callsite.getLineNumber(), callsite.getColumnNumber());
 //   })
-
+//   counter++;
 //   if (num === 0) return 0;
 //   if (num === 1) return 1;
 //   return fib(num - 1) + fib(num - 2);
 // }
+
 console.log(fib);
 fib(5);
