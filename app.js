c28173c08ecd3840b651ce985396b3fc6851f238
Karen <karenpenglabs@gmail.com>
2015-02-13 18:03:51 -0500

    push code before kids touching my computer

--- app.js
+++ app.js
@@ -1,70 +1,110 @@
 var stack = require('callsite');
-//eventally it's just a json object to control the animation???
 
-//what is a recursive function?
+//1.inject callsite
+//2.call it
+//3.get callste data
+//4.analysis the data
+//5.form an object
+//6.use the object to animate
 
-//a function that calls itself?
-
-//looks like
-//function xxx(){
-//
-//xxx()
-//
-//}
-//
 var counter = 0;
-var callSiteCode = [
-  " stack().forEach(function (callsite, i) { ",
-  "console.log('%s counter: %s, level: %s, type: %s, function: %s, method: %s, filename: %s, line: %s, column: %s', ",
-  "new Array(i + 1).join(' '), counter, i, callsite.getTypeName(), callsite.getFunctionName(), callsite.getMethodName(), callsite.getFileName(), ",
-  "callsite.getLineNumber(), callsite.getColumnNumber()); ",
-  "})",
-  "counter++;"
-].join('\n');
 
-// function joinCallSiteCode() {
+function callSite() {
+  stack().forEach(function (callsite, i) {
+    console.log('%s counter: %s, level: %s, type: %s, function: %s, method: %s, filename: %s, line: %s, column: %s',
+      new Array(i + 1).join(' '), counter, i, callsite.getTypeName(), callsite.getFunctionName(), callsite.getMethodName(), callsite.getFileName(),
+      callsite.getLineNumber(), callsite.getColumnNumber());
+  });
+  counter++;
+}
+
+//function below scans the function string line by line
+//but i'm not going to do it any more
+//b/c let's assume there's only one recursive function on the outtest layer
+//make our lives easier :)
+// function inject(strArray) {
+//   var functionRe = /function\s\S*\(.*?\)\{/;
+
 //   var endStr = '';
-//   endStr += '\n';
-//   callSiteCode.forEach(function (code) {
-//     endStr += code.concat('\n');
+//   strArray.forEach(function (str) {
+//     if (str.match(functionRe) !== null) {
+//       var functionLine = str.match(functionRe)[0];
+//       console.log(functionLine);
+
+//       var postStr = str.split(functionRe);
+//       //console.log(postStr);
+//       postStr = functionLine.concat(postStr[0]).concat('\n').concat(callSite.toString()).concat(postStr[1]).concat('\n');
+//       endStr += postStr;
+//     } else {
+//       endStr += str.concat('\n');
+//     }
 //   });
+//   console.log(endStr);
 //   return endStr;
 // }
 
-function inject(strArray) {
-  var functionRe = /function\s\S*\(\S*\)\{/;
-  //var postStr = functionRe.exec(str);
+function inject(str) {
+  var functionRe = /function\s\S*\s*\(.*?\)\s*\{/;
   var endStr = '';
-  strArray.forEach(function (str) {
-    if (str.match(functionRe) !== null) {
-      var functionLine = str.match(functionRe)[0];
-      //console.log(functionLine)
-      var postStr = str.split(functionRe);
-      //console.log(postStr)
-      postStr = functionLine.concat(postStr[0]).concat('\n').concat(callSiteCode).concat(postStr[1]).concat('\n');
-      endStr += postStr;
-    } else {
-      endStr += str.concat('\n');
-    }
+  if (str.match(functionRe) !== null) {
+    var functionLine = str.match(functionRe)[0];
+    //console.log('functionLine ' + functionLine);
+    //do something here for the firstLine
+    console.log('functionLine ' + functionLine);
+    functionLine = transformFunction(functionLine);
+    // functionLine.
+    var postStr = str.split(functionRe);
+    var latterPart = postStr[1]
+      // concat(postStr[0]).
+      // concat('\n').
+      // concat(callSite.toString()).
+      // concat(postStr[1]).
+      // concat('\n');
 
-  });
-  // var functionLine = str.match(functionRe)[0];
-  // var postStr = str.split(functionRe);
-  // //console.log(postStr)
-  // postStr = functionLine.concat(postStr[0]).concat(callSiteCode).concat(postStr[1]);
-  //console.log(postStr);
+  } else {
+    console.log('no function (ಥ_ಥ)');
+    endStr += str;
+  }
   console.log(endStr);
   return endStr;
 }
 
-function quickTest() {
-  var str = 'sddg'
-  var functionRe = /function\s\S*\(\S*\)\{/
-  if (str.match(functionRe) !== null) {
-    var functionLine = str.match(functionRe)[0];
-    console.log(functionLine)
-    var postStr = str.split(functionRe);
-    console.log(postStr)
+//okay, now this is the wacky part
+function transformFunction(str) {
+  var newFunctionLine = '';
+
+  var functionNameRe1 = /var\s\w+\s*/;
+  var functionNameRe2 = /function\s\w+\s*/;
+  var functionName = '';
+
+  if (str.match(functionNameRe1) !== null) {
+    functionName = str.match(functionNameRe1)[0].replace(/var/, '');
+  }
+  if (str.match(functionNameRe2) !== null) {
+    functionName = str.match(functionNameRe2)[0].replace(/function/, '');
+  }
+  console.log('functionName ' + functionName);
+
+  var param = '';
+  var paramRe = /\(\w+\)/;
+  if (str.match(paramRe) !== null) {
+    param = str.match(paramRe)[0].replace('(', '').replace(')', '');
+  }
+  console.log('param ' + param);
+
+  newFunctionLine = 'var '.concat(functionName).concat(' = new Function (').concat(param).concat(', ')
+
+  return newFunctionLine;
+
+}
+
+function getRidOfBracket(str) {
+  for (var i = str.length; i >= 0; i--) {
+    if (str[i] === '}') {
+
+      break;
+    }
+
   }
 }
 
@@ -79,35 +119,24 @@ function evaluate(str) {
   }
 }
 
-// function fib(num) {
-//   if (num === 0) return 0;
-//   if (num === 1) return 1;
-//   return fib(num - 1) + fib(num - 2);
-// }
+function fib(num) {
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+  return fib(num - 1) + fib(num - 2);
+}
 
-var fibStr = [
-  "function fib(num){",
-  "if (num === 0) return 0;",
-  "if (num === 1) return 1;",
-  "return fib(num - 1) + fib(num - 2);",
-  "}"
-]
+// var fibStr = [
+//   "function fib(num){",
+//   "if (num === 0) return 0;",
+//   "if (num === 1) return 1;",
+//   "return fib(num - 1) + fib(num - 2);",
+//   "}"
+// ]
 
 //console.log(fib)
+//console.log(fib.toString())
 
-evaluate(inject(fibStr));
+evaluate(inject(fib.toString()));
 
-// function fib(num) {
-//   stack().forEach(function (callsite, i) {
-//     console.log('%s counter: %s, level: %s, type: %s, function: %s, method: %s, filename: %s, line: %s, column: %s',
-//       new Array(i + 1).join(' '), counter, i, callsite.getTypeName(), callsite.getFunctionName(), callsite.getMethodName(), callsite.getFileName(),
-//       callsite.getLineNumber(), callsite.getColumnNumber());
-//   })
-//   counter++;
-//   if (num === 0) return 0;
-//   if (num === 1) return 1;
-//   return fib(num - 1) + fib(num - 2);
-// }
-
-console.log(fib);
-fib(5);
+//console.log(fib);
+//fib(5);
